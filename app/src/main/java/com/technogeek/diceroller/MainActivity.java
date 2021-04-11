518c4a2dce882f9fcc214422435ae33e4c45fca3
TranBaKhoa1999 <ohwhynotme1999@gmail.com>
2021-04-11 16:40:11 +0700

    roll dice oke

---
+++ app/src/main/java/com/technogeek/diceroller/MainActivity.java
@@ -0,0 +1,40 @@
+package com.technogeek.diceroller;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+
+import java.util.Random;
+
+public class MainActivity extends AppCompatActivity {
+
+    ImageView diceImage;
+    Random random = new Random();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        diceImage = findViewById(R.id.dice_image);
+
+        diceImage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                rotateDice();
+            }
+        });
+    }
+
+    private void rotateDice() {
+        int i = random.nextInt(5)+1;
+        Animation anim = AnimationUtils.loadAnimation(this, R.anim.rotate);
+        diceImage.startAnimation(anim);
+        int res = getResources().getIdentifier("dice" + i, "drawable",getPackageName());
+        diceImage.setImageResource(res);
+    }
+}
