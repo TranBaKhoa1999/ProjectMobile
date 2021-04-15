5311bae01a5ae1093afb51f236a97fd9faf530ad
Tài Vương <taivuongduy2@gmail.com>
2021-04-15 15:16:54 +0700

    Done Layout Select

--- app/src/main/java/com/technogeek/diceroller/SelectCharacterActivity.java
+++ app/src/main/java/com/technogeek/diceroller/SelectCharacterActivity.java
@@ -2,7 +2,9 @@ package com.technogeek.diceroller;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.pm.ActivityInfo;
 import android.os.Bundle;
+import android.view.View;
 
 public class SelectCharacterActivity extends AppCompatActivity {
 
@@ -10,5 +12,9 @@ public class SelectCharacterActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_select_character);
+        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
+    }
+    public void CloseActivity(View view) {
+        onBackPressed();
     }
 }
