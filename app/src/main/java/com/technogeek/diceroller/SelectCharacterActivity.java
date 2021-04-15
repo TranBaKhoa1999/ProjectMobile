d9164340a9d277d8c282f50e9f33cd190a8ba1db
Tài Vương <taivuongduy2@gmail.com>
2021-04-15 13:47:33 +0700

    thêm activity chọn nhân vật + upload character image

---
+++ app/src/main/java/com/technogeek/diceroller/SelectCharacterActivity.java
@@ -0,0 +1,14 @@
+package com.technogeek.diceroller;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SelectCharacterActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_select_character);
+    }
+}
